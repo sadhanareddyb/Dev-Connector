be120ee71665a29805d9901219776279ca8b2da8
sadhanareddyb <butreddysadhana@gmail.com>
2020-07-28 14:48:47 +0530

    user model and route created

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.export={
-    mongoURI="mongodb+srv://sadhana:PSJLk2a983avLuRQ@ecommerceatlas-mibcz.mongodb.net/DevConnector?retryWrites=true&w=majority"
-
-};
--- server.js
+++ server.js
@@ -1,22 +1,37 @@
-const express=require('express');
-const mongoose=require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require('path');
 
-const app=express();
+const bodyParser = require("body-parser");
 
-const db=require('./config/keys').mongoURI;
+require("dotenv");
+
+
+// app
+const app = express();
+
+// db
 mongoose
-.connect(db)
-.then(()=>console.log("MongoDB Connected"))
-.catch(err=>console.log(err));
+    .connect(process.env.MONGOURI, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true 
+    })
+    .then(() => console.log("DB Connected"));
+
+// Init Middleware
+app.use(express.json());
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 
-app.get('/',(req,res)=>res.send('Hello'));
 
+const Port = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-
-
-
+app.listen(Port, () => {
+    console.log(`Server is running on port ${Port}`);
+});
